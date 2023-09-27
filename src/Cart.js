4aef5100c61658b8bb29f2aca58b795abcebdcdc
rahulpathak1501 <rahul150101130015@gmail.com>
2023-09-27 17:17:09 +0530

    added delete function

--- src/Cart.js
+++ src/Cart.js
@@ -3,12 +3,82 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 99,
+          title: "Watch",
+          qty: 10,
+          img: "",
+          id: 1,
+        },
+        {
+          price: 999,
+          title: "Mobile Phone",
+          qty: 14,
+          img: "",
+          id: 2,
+        },
+        {
+          price: 9999,
+          title: "Laptop",
+          qty: 19,
+          img: "",
+          id: 3,
+        },
+      ],
+    };
+  }
+
+  handleIncrease = (product) => {
+    //console.log(product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    products[index].qty += 1;
+    this.setState({
+      products,
+    });
+  };
+
+  handleDecrease = (product) => {
+    //console.log(product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    if (products[index].qty === 1) {
+      return;
+    }
+    products[index].qty -= 1;
+    this.setState({
+      products,
+    });
+  };
+  handleDelete = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    products.splice(index, 1);
+    this.setState({
+      products,
+    });
+    console.log(products)
+  };
+
   render() {
+    const { products } = this.state;
     return (
       <div className="cart">
-        <CartItem />
-        <CartItem />
-        <CartItem />
+        {products.map((product) => {
+          return (
+            <CartItem
+              product={product}
+              key={product.id}
+              onClickIncrease={this.handleIncrease}
+              onClickDecrease={this.handleDecrease}
+              onClickDelete={this.handleDelete}
+            />
+          );
+        })}
       </div>
     );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,43 +1,33 @@
 import React from "react";
 
 class CartItem extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      price: 999,
-      title: "Mobile Phone",
-      qty: 1,
-      img: "",
-    };
-  }
-
-  increse = () => {
-    console.log(this.state.qty);
-    // (this.state.qty += 1);
-    // this type of update is called object form
-    // this.setState({
-    //   qty: this.state.qty + 1,
-    // });
-    // there is anothe type of udate is there that is function form
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1,
-      };
-    });
-    // this type we use when we might need the previous state again.
-  };
-  decrease = () => {
-    const {qty}=this.state.qty;
-    if(qty===0){
-        return;
-    }
-    this.setState({
-      qty: this.state.qty - 1,
-    });
-  };
+  // increse = () => {
+  //   console.log(this.state.qty);
+  //   // (this.state.qty += 1);
+  //   // this type of update is called object form
+  //   // this.setState({
+  //   //   qty: this.state.qty + 1,
+  //   // });
+  //   // there is anothe type of udate is there that is function form
+  //   this.setState((prevState) => {
+  //     return {
+  //       qty: prevState.qty + 1,
+  //     };
+  //   });
+  //   // this type we use when we might need the previous state again.
+  // };
+  // decrease = () => {
+  //   const {qty}=this.state.qty;
+  //   if(qty===0){
+  //       return;
+  //   }
+  //   this.setState({
+  //     qty: this.state.qty - 1,
+  //   });
+  // };
 
   render() {
-    const { price, title, qty } = this.state;
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -51,18 +41,19 @@ class CartItem extends React.Component {
             {/* Buttons */}
             <img
               alt="decrese"
-              onClick={this.decrease}
+              onClick={() => this.props.onClickDecrease(this.props.product)}
               className="action-icons"
               src="https://img.icons8.com/ios/25/minus.png"
             />
             <img
               alt="increse"
-              onClick={this.increse}
+              onClick={() => this.props.onClickIncrease(this.props.product)}
               className="action-icons"
               src="https://img.icons8.com/ios/25/plus--v1.png"
             />
             <img
               alt="delete"
+              onClick={() => this.props.onClickDelete(this.props.product)}
               className="action-icons"
               src="https://img.icons8.com/material-outlined/24/filled-trash.png"
             />
